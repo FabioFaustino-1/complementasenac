9b03f33ae8fa031c5ef91adbe154a9059dd71c87
FabioFaustino-1 <fabiofaustino010@gmail.com>
2026-03-23 11:48:53 -0300

    implementação do firebase

---
+++ frontend/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4oWuOGDjJmsBXdy9WGWEB_Q_vapH0h4I",
+  authDomain: "pi-3-286ed.firebaseapp.com",
+  projectId: "pi-3-286ed",
+  storageBucket: "pi-3-286ed.firebasestorage.app",
+  messagingSenderId: "808575862472",
+  appId: "1:808575862472:web:0159933b1aaad7069e07fd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
