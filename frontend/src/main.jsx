19aa61dce0baf9f66da88ee3e9ef12ed027a1808
FabioFaustino-1 <fabiofaustino010@gmail.com>
2026-03-23 16:30:38 -0300

    Refactor main.jsx to use App component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-// Olha como o caminho ficou limpo agora, só com o ponto e barra:
-import Aluno from "./Aluno.jsx"; 
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './pages/login/App'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Aluno />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
