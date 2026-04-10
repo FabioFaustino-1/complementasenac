d491bc308f7c0aac8391d3d0284ce74fab96d587
FabioFaustino-1 <fabiofaustino010@gmail.com>
2026-04-10 17:33:25 -0300

    Update teste.rb

--- docs/teste.rb
+++ docs/teste.rb
@@ -10,4 +10,11 @@ puts "helloworld"
 /*
 $env:JAVA_HOME="C:\Users\aluno\AppData\Local\Programs\Eclipse Adoptium\jdk-17.0.18.8-hotspot"
 $env:Path="$env:JAVA_HOME\bin;$env:Path"
+
+
+IMPORTANTE::
+css das paginas dando conflito<> (devido as classes globais em cada um)
+terminar rotas e paginas<> (faltam telas a serem implementadas)
+enfim retomar o backend<> (preciso de algumas funções em telas distintas)
+
 */
